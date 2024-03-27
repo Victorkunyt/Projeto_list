aa8bfe582779f27c64c809476b6dbc0878716a22
Victor <Victorgates0@gmail.com>
2024-03-27 12:12:33 -0300

    Bug - Ajustando o autenticador

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,19 +1,19 @@
 import { FastifyRequest, FastifyReply } from "fastify";
 import jwt from "jsonwebtoken";
 
-export function AuthMiddleware(req: FastifyRequest, res: FastifyReply) {
-	const auth = req.headers["authorization"];
+export async function AuthMiddleware(req: FastifyRequest, res: FastifyReply, next: () => void) {
+    const auth = req.headers["authorization"];
 
-	if (!auth || typeof auth !== "string") {
-		return res.code(401).send({ message: "Token não passado" });
-	}
+    if (!auth || typeof auth !== "string") {
+        return res.code(401).send({ message: "Token não passado" });
+    }
 
-	const [, token] = auth.split(" ");
+    const [, token] = auth.split(" ");
 
-	try {
-		jwt.verify(token, "suaChaveSecreta");
-		return;
-	} catch (error) {
-		return res.code(401).send({ message: "Token de autenticação inválido ou expirado" });
-	}
+    try {
+        jwt.verify(token, "suaChaveSecreta");
+        next(); 
+    } catch (error) {
+        return res.code(401).send({ message: "Token de autenticação inválido ou expirado" });
+    }
 }
