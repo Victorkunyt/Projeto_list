9f6581922dac2e76a2aab21c173cb5f096899eda
Victor <Victorgates0@gmail.com>
2024-06-11 16:43:40 -0300

    Metricas Performance Vercel

--- Backend/src/server.ts
+++ Backend/src/server.ts
@@ -9,34 +9,38 @@ import { routesNotification } from "./routes/Notification";
 import { routesPdf } from "./routes/GeneratePdf";
 import { routesNewpassword } from "./routes/Pass";
 import { routesSendEmail } from "./routes/SendEmail";
+import { injectSpeedInsights } from '@vercel/speed-insights';
+import fastifyJwt from '@fastify/jwt';
 
-
-
-const app = fastify({logger: true});
+const app = fastify({ logger: true });
 const PORT = parseInt(`${process.env.PORT || 3333}`);
 
-// Plugin do CORS
- app.register(cors);
+const configureServer = async () => {
+  // Plugin do CORS
+  await app.register(cors);
 
-// Registrando todas as Rotas
- const route = [routesUsers, routesTask, routesCategory, routesRefreshToken, routesShared, routesNotification, routesPdf, routesNewpassword,routesSendEmail];
+  // Plugin JWT
+  await app.register(fastifyJwt, {
+    secret: 'supersecret'
+  });
 
- route.forEach(rotas => {
-   app.register(rotas);
- })
+  // Registrando todas as Rotas
+  const routes = [routesUsers, routesTask, routesCategory, routesRefreshToken, routesShared, routesNotification, routesPdf, routesNewpassword, routesSendEmail];
 
-// Plugin JWT
-app.register(require('@fastify/jwt'), {
-  secret: 'supersecret'
-});
+  for (const route of routes) {
+    await app.register(route);
+  }
 
-app.setErrorHandler((error, request, reply) => {
-  reply.code(400).send({message : error.message});
-});
+  app.setErrorHandler((error, request, reply) => {
+    reply.code(400).send({ message: error.message });
+  });
+};
 
 const start = async () => {
   try {
+    await configureServer();
     await app.listen({ port: PORT });
+    injectSpeedInsights();
   } catch (err) {
     console.error('Ocorreu um erro ao iniciar o servidor:', err);
     process.exit(1);
