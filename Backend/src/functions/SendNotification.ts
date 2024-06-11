c6fc1a235ed5e95d006bd86b5f5a5eba2ee0167f
Victor <Victorgates0@gmail.com>
2024-06-11 18:19:43 -0300

    Ajustes Notification

--- Backend/src/functions/SendNotification.ts
+++ Backend/src/functions/SendNotification.ts
@@ -1,23 +1,40 @@
 import { PrismaClient } from "@prisma/client";
 
 export async function registerNotification(prisma: PrismaClient, type: string, recipientId: string): Promise<void> {
+    
     if (!recipientId.trim() || recipientId.length !== 24 || typeof recipientId !== 'string') {
         throw new Error('Erro ao registrar notificação: recipientId inválido.');
     }
 
     try {
+        // Registrar a nova notificação
         const newNotification = await prisma.notification.create({
             data: {
-                type,
-                recipientId
+              type: type,
+              recipientId: recipientId,
+              expiresAt: new Date(Date.now() + 1 * 60 * 1000) // Expira em 1 minuto
             }
         });
-        
+          
         console.log('Notificação registrada com sucesso:', newNotification);
+
+        // Limpar notificações expiradas
+        const now = new Date();
+        const result = await prisma.notification.deleteMany({
+            where: {
+                expiresAt: {
+                    lt: now // Excluir notificações onde expiresAt é menor que a data e hora atuais
+                }
+            }
+        });
+
+        console.log(`Notificações expiradas removidas: ${result.count}`);
+        
     } catch (error) {
         throw new Error('Erro ao registrar notificação: erro ao criar a notificação no Prisma Client');
     }
 }
+
 
 
 
--- Backend/src/services/Notifications/Notifications_service.ts
+++ Backend/src/services/Notifications/Notifications_service.ts
@@ -2,31 +2,32 @@ import { PrismaClient } from "@prisma/client";
 import { LineNotification } from "../../types/Notification_types";
 import { NotificationId } from "../../validators/Notification/Notification_validator";
 
-
 class GetNotificationsService {
   private prisma: PrismaClient;
 
   constructor(prisma: PrismaClient) {
     this.prisma = prisma;
   }
-  async execute(userData: LineNotification) {
-    NotificationId(userData)
-    const notification = await this.prisma.notification.findMany({
 
+  async execute(userData: LineNotification) {
+    NotificationId(userData);
+
+    const now = new Date();
+    const notification = await this.prisma.notification.findMany({
       where: {
-        recipientId: userData.userId
+        recipientId: userData.userId,
+        OR: [
+          { expiresAt: null },  // Inclui notificações que não têm expiração definida
+          { expiresAt: { gt: now } }  // Inclui notificações que não expiraram ainda
+        ]
       }
     });
-    
-    if(notification.length === 0) {
-      return { message: "Nenhuma Notificação encontrada" };
 
+    if (notification.length === 0) {
+      return { message: "Nenhuma Notificação encontrada" };
     }
 
-
-    return {notification}
-
- 
+    return { notification };
   }
 }
 
